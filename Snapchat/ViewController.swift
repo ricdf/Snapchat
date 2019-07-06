834c4e86157f4bd0bad3e53936541621062ca0d9
Ricardo Cavalcante <ricodf@gmail.com>
2019-07-05 22:21:44 -0300

    Initial Commit

---
+++ Snapchat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Snapchat
+//
+//  Created by Ricardo Cavalcante on 05/07/19.
+//  Copyright © 2019 Ricardo Cavalcante. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
